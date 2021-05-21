167a90bd3805a2ceb542383bb744c68e104ae580
richardkanai123 <richardpoetgamer@gmail.com>
2021-05-21 07:46:36 +0300

    end of the mockup site for a coffee shop front end only

---
+++ app.js
@@ -0,0 +1,11 @@
+// todo 1:dark mode 2: flipping cards for menu 3.Tilt js for cards
+const nav = document.querySelector('nav')
+const bars = document.querySelector('#bar');
+bars.addEventListener('click', ()=>{
+    nav.classList.toggle('open');
+})
+
+
+nav.addEventListener('click', ()=>{
+    nav.classList.toggle('open');
+})
